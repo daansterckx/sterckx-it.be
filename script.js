90f8beded251bcb99c5ee195d3d9f6969782a44e
daansterckx <sterckxdaan1@gmail.com>
2025-04-09 23:31:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// JavaScript
+function openModal1() {
+    document.getElementById("myModal1").style.display = "block";
+}
+  function closeModal1() {
+    document.getElementById("myModal1").style.display = "none";
+}
+  function openModal2() {
+    document.getElementById("myModal2").style.display = "block";
+}
+  function closeModal2() {
+    document.getElementById("myModal2").style.display = "none";
+}
+  function openModal3() {
+    document.getElementById("myModal3").style.display = "block";
+} 
+  function closeModal3() {
+    document.getElementById("myModal3").style.display = "none";
+}
+  function openModal4() {
+    document.getElementById("myModal4").style.display = "block";
+}
+  function closeModal4() {
+    document.getElementById("myModal4").style.display = "none";
+}
+function openModal5() {
+  document.getElementById("myModal5").style.display = "block";
+}
+function closeModal5() {
+  document.getElementById("myModal5").style.display = "none";
+}
+function openModal6() {
+  document.getElementById("myModal6").style.display = "block";
+}
+function closeModal6() {
+  document.getElementById("myModal6").style.display = "none";
+}
